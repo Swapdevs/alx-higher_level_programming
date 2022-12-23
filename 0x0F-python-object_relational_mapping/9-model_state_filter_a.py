b61cca8e66880dc3fa786eb5ac6878d444002a65
Christopher Akinsanmi <christopher.akinsanmi@gmail.com>
2022-12-23 01:14:40 +0100

    Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,21 +1,33 @@
 #!/usr/bin/python3
-# Lists all State objects that contain the letter a
-# from the database hbtn_0e_6_usa.
-# Usage: ./9-model_state_filter_a.py <mysql username> /
-#                                    <mysql password> /
-#                                    <database name>
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import State
+# lists all State objects that contain the letter a from a database
+
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    from model_state import Base, State
+    from sys import argv
+    import sqlalchemy
+    from sqlalchemy.engine.url import URL
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
 
-    for state in session.query(State).order_by(State.id):
-        if "a" in state.name:
-            print("{}: {}".format(state.id, state.name))
+    mysql = {'drivername': 'mysql+mysqldb',
+             'host': 'localhost',
+             'port': '3306',
+             'username': argv[1],
+             'password': argv[2],
+             'database': argv[3],
+             }
+
+    url = URL(**mysql)
+
+    engine = create_engine(url, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+
+    query = session.query(State).filter(State.name.like('%a%'))\
+                                .order_by(State.id)
+    for r in query.all():
+        print("{}: {}".format(r.id, r.name))
+
+    session.close()
